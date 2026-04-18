95149a73fabe822eae0f790d296cfc72a3b15e92
Marvel S <aamarvsr@gmail.com>
2026-04-18 20:36:20 +0700

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://lnxydqlesrttmwzobzfw.supabase.co";
+const SUPABASE_KEY = "sb_publishable_lSc0Ej8BWAp4ZXwfJZdQww_zg0kY790";
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
